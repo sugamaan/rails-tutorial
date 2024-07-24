3ad00c0a0d65f994a2a7fba973f191c2921cf666
sugama <reiji.sugama@gmail.com>
2024-07-24 15:41:13 +0900

    remember_digestカラムの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,30 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  # validates :name,  presence: true, length: { maximum: 50 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, length: { maximum: 255 },
+  #                   format: { with: VALID_EMAIL_REGEX },
+  #                   uniqueness: true
+  # validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+  attr_accessor :remember_token
 
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    # 属性にアクセスするためにselfを記述する
+    self.remember_token = User.new_token
+    # 記憶ダイジェストを更新する
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_23_084559) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_24_024059) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
