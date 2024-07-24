659763e1be1a17c485bb5ceea1bfaf395152a600
sugama <reiji.sugama@gmail.com>
2024-07-24 15:54:47 +0900

    rails generate mailer UserMailer account_activation password_reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   # validates :name,  presence: true, length: { maximum: 50 }
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # validates :email, presence: true, length: { maximum: 255 },
@@ -7,7 +9,6 @@ class User < ApplicationRecord
   #                   uniqueness: true
   # validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
-  attr_accessor :remember_token
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
@@ -27,4 +28,17 @@ class User < ApplicationRecord
     # 記憶ダイジェストを更新する
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  private
+
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_024059) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_24_064851) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_024059) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
 end
