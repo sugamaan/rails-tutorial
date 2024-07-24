fbe004f999986672c5273c468e518d169d205c26
sugama <reiji.sugama@gmail.com>
2024-07-24 16:07:10 +0900

    rails generate migration add_reset_to_users reset_digest:string \ > reset_sent_at:datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_064851) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_24_070703) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_064851) do
     t.string "remember_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
   end
 
 end
